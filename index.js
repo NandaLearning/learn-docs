6011e5b8c7fde9e2abb0f5fe53b897f4c76bb79c
Nanda <anandalukman01@gmail.com>
2024-07-13 20:34:32 +0700

    first commit

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-import express from "express"
-import cors from "cors"
-import { sekolahController } from "./controller/sekolahController.js"
-import  YAML from "yamljs"
-import swaggerUiExpress from "swagger-ui-express"
+import express from 'express';
+import cors from 'cors';
+import YAML from 'yamljs';
+import swaggerUiExpress from 'swagger-ui-express';
+import path from 'path'; // Import path dari Node.js untuk manipulasi path
 
-const app = express()
-const port = 3000
+import { sekolahController } from './controller/sekolahController.js';
 
-app.use(cors())
-app.use(express.json())
+const app = express();
+const port = 3000;
 
-const swagger = YAML.load("config.yml")
-app.use("/api",swaggerUiExpress.serve,swaggerUiExpress.setup(swagger))
+app.use(cors());
+app.use(express.json());
 
-app.get("/sekolah", sekolahController.getAllSekolah)
-app.get("/sekolah/:id", sekolahController.getSekolahById)
-app.post("/sekolah", sekolahController.createSekolah)
-app.put("/sekolah/:id", sekolahController.updateSekolahById)
-app.delete("/sekolah/:id", sekolahController.deleteSekolahById)
+const swaggerPath = path.join('config.yml');
 
+const swaggerDocument = YAML.load(swaggerPath);
+
+app.use('/api', swaggerUiExpress.serve, swaggerUiExpress.setup(swaggerDocument));
+
+// Routes untuk kontroler sekolah
+app.get('/sekolah', sekolahController.getAllSekolah);
+app.get('/sekolah/:id', sekolahController.getSekolahById);
+app.post('/sekolah', sekolahController.createSekolah);
+app.put('/sekolah/:id', sekolahController.updateSekolahById);
+app.delete('/sekolah/:id', sekolahController.deleteSekolahById);
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:3000/`)
-})
+    console.log(`Server running on http://localhost:${port}/`);
+});
